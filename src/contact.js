73f63acc95b8bfeeeb27121ba34afe82e30d6f7d
Tim Yu <yuyu.tim@gmail.com>
2020-12-24 14:52:56 -0800

    Implemented code for contact page

---
+++ src/contact.js
@@ -0,0 +1,26 @@
+const loadContactPage = function(elementID) {
+  let contents = document.querySelector(elementID);
+
+  contents.innerHTML = "";
+
+  let heading = document.createElement("h1");
+  heading.textContent = "Contact Us";
+
+  let addressHeading = document.createElement("h2");
+  addressHeading.textContent = "Address";
+  contents.appendChild(addressHeading);
+
+  let address = document.createElement("p");
+  address.innerHTML = "Good Restaurant<br>123 First Street<br>Anytown, CA 99999";
+  contents.appendChild(address);
+
+  let phoneNumberHeading = document.createElement("h2");
+  phoneNumberHeading.innerHTML = "Phone number"
+  contents.appendChild(phoneNumberHeading);
+
+  let phoneNumber = document.createElement("p");
+  phoneNumber.textContent = "(123) 456-7890";
+  contents.appendChild(phoneNumber);
+}
+
+export { loadContactPage as default };
